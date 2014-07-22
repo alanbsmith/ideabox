ce146fe3678f5222a0a5bf9184a4e213bf152296
Alan Smith <a.bax.smith@gmail.com>
2014-07-22 10:56:06 -0600

    adds a working database

--- app.rb
+++ app.rb
@@ -1,8 +1,24 @@
 require 'bundler'
+require './idea'
 Bundler.require
 
 class IdeaBoxApp < Sinatra::Base
-  get '/' do
-    "Hello, World!"
+  not_found do
+    erb :error
   end
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb :index, locals: {ideas: Idea.all}
+  end
+
+  post '/' do
+    idea = Idea.new(params['idea_title'], params['idea_description'])
+    idea.save
+    redirect '/'
+  end
+
 end
