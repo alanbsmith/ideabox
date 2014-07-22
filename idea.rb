7926d8bcb45417a1dbd5cf0463d8e1b97a1fc801
Alan Smith <a.bax.smith@gmail.com>
2014-07-22 11:30:36 -0600

    adds editing/updating feature

--- idea.rb
+++ idea.rb
@@ -20,6 +20,29 @@ class Idea
     end
   end
 
+  def self.delete(position)
+    database.transaction do
+      database['ideas'].delete_at(position)
+    end
+  end
+
+  def self.find(id)
+    raw_idea = find_raw_idea(id)
+    new(raw_idea[:title], raw_idea[:description])
+  end
+
+  def self.find_raw_idea(id)
+    database.transaction do
+      database['ideas'].at(id)
+    end
+  end
+
+  def self.update(id, data)
+    database.transaction do
+      database['ideas'][id] = data
+    end
+  end
+
   def save
     database.transaction do |db|
       db['ideas'] ||= []
